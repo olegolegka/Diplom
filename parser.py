f227718209e28a75691ffada5ede5e9921f825d5
Oleg Babchenok <Oleg1Babchenok1@gmail.com>
2022-02-05 17:52:45 +0300

    OOP cleaned

--- parser.py
+++ parser.py
@@ -2,47 +2,63 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import time
-from tkinter import ttk
+from tkinter import ttk, Frame
 from ttkthemes import ThemedTk
 block_ulrs = []
-def clicked():
-    separator = int(Sep_txt.get())
-    delay = int(delay_combobox.get())
-    for i in range (0, separator, 1):
-        time.sleep(delay)
-        url = Link_txt.get()+str(i)
-        print(url)
-        q = requests.get (url)
-        result = q.content
-        soup = BeautifulSoup (result, 'lxml')
-        blocks = soup.find_all (class_=Class_txt.get())
-
-        for block in blocks:
-            block_url = block.get ('href')
-            block_ulrs.append ( block_url)
-
-    with open ('test.csv', 'w') as file:
-        for line in block_ulrs:
-            file.write (f'{line}\n')
 
 
-window = ThemedTk(theme = 'yaru')
-window.geometry("1200x600")
-window.wm_iconbitmap('myicon.ico')
-window.title("Парсер данных")
-Link_label = ttk.Label(window, text="Ссылка")
-Link_label.grid(column=0, row=0,padx =20,pady=20 )
-Link_txt = ttk.Entry(window,width=80)
-Link_txt.grid(column=1, row=0)
-Sep_txt = ttk.Entry(window, width=5)
-Sep_txt.grid(column=2,row=0)
-delay_combobox = ttk.Combobox(window,width = 4,values = [0,3,5,10,15])
-delay_combobox.current(0)
-delay_combobox.grid(column=3,row=0,)
-Link_label = ttk.Label(window, text="Класс")
-Link_label.grid(column=0, row=1,padx =20,pady=20 )
-Class_txt = ttk.Entry(window,width=80)
-Class_txt.grid(column=1, row=1)
-Run_btn = ttk.Button(window, text="Клик!", command=clicked)
-Run_btn.grid(column=4, row=0)
-window.mainloop()
+class Parser(Frame):
+    def __init__(self):
+        self.InitUi ()
+        super ().__init__ ()
+
+
+    def InitUi(self):
+        self.window = ThemedTk (theme='yaru')
+        self.window.geometry ("1200x600")
+        self.window.wm_iconbitmap ('myicon.ico')
+        self.window.title ("Парсер данных")
+        self.Link_label = ttk.Label (self.window, text="Ссылка")
+        self.Link_label.grid (column=0, row=0, padx=20, pady=20)
+        self.Link_txt = ttk.Entry (self.window, width=80)
+        self.Link_txt.grid (column=1, row=0)
+        self.Sep_txt = ttk.Entry (self.window, width=5)
+        self.Sep_txt.grid (column=2, row=0)
+        self.delay_combobox = ttk.Combobox (self.window, width=4, values=[0, 3, 5, 10, 15])
+        self.delay_combobox.current (0)
+        self.delay_combobox.grid (column=3, row=0, )
+        self.Link_label = ttk.Label (self.window, text="Класс")
+        self.Link_label.grid (column=0, row=1, padx=20, pady=20)
+        self.Class_txt = ttk.Entry (self.window, width=80)
+        self.Class_txt.grid (column=1, row=1)
+        self.Run_btn = ttk.Button (self.window, text="Клик!", command=self.clicked)
+        self.Run_btn.grid (column=4, row=0)
+        self.window.mainloop ()
+    def clicked(self):
+        '''
+        Функция обработчик кнопки получения всех ссылок на товары
+        '''
+        print(self.Sep_txt.get())
+        separator = int(self.Sep_txt.get ())
+        delay = int (self.delay_combobox.get ())
+        for i in range (0, separator, 1):
+            time.sleep (delay)
+            url = self.Link_txt.get () + str (i + 1)
+            print (url)
+            q = requests.get (url)
+            result = q.content
+            soup = BeautifulSoup (result, 'lxml')
+            blocks = soup.find_all (class_=self.Class_txt.get ())
+
+            for block in blocks:
+                block_url = block.get ('href')
+                block_ulrs.append (block_url)
+
+        with open ('test.csv', 'w') as file:
+            for line in block_ulrs:
+                file.write (f'{line}\n')
+def main():
+    app = Parser()
+
+if __name__ == '__main__':
+    main()
